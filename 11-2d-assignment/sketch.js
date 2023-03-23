c4a1a407295d9b3c6cf6d264d0d3a0472de92e95
Blite33 <w.harris@hotmail.ca>
2023-03-23 15:19:33 -0600

    commit 1500 real

---
+++ 11-2d-assignment/sketch.js
@@ -0,0 +1,25 @@
+// IDK Yet
+// William Harris
+// March 23rd 2023
+// latest v23/03/23
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+// Goals:
+// Get 2d grid set up
+// maybe game of life with some extra rules
+// - Like having a harm block
+// - Don't take to much inspiration from the youtube video
+// Maybe implement a 2d version of the Dilithium vector encryption.
+// A* algorithm sounds pretty good
+// - Rewatch the 3Blue1Brown video?
+// Make a graphing calculator? That sounds a little complicated.
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
