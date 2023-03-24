51c43512b3513204ba41f29106c9e733a53e1da1
Blite33 <w.harris@hotmail.ca>
2023-03-24 15:19:41 -0600

    assignment thinking

--- 11-2d-assignment/sketch.js
+++ 11-2d-assignment/sketch.js
@@ -14,12 +14,24 @@
 // Maybe implement a 2d version of the Dilithium vector encryption.
 // A* algorithm sounds pretty good
 // - Rewatch the 3Blue1Brown video?
+// - Actually, it's https://www.youtube.com/watch?v=A60q6dcoCjw
 // Make a graphing calculator? That sounds a little complicated.
+
+let grid;
+let fillArray;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
 }
 
 function draw() {
   background(220);
+}
+
+function fill2dArray() {
+  // Do i need an array that includes different objects?
+  fillArray = {
+
+  }
 }
